fa52c2907d16effcc2ce006b096ff5605b34b299
Shane Morgan <shane@cd2solutions.co.uk>
2017-09-10 11:10:53 +0100

    course teaser better and better

--- components/background_image.jsx
+++ components/background_image.jsx
@@ -34,10 +34,12 @@ export default class BackgroundImage extends React.Component {
   }
 
   render() {
-    const { className } = this.props
+    const { className, children } = this.props
 
     return (
-      <div className={className} />
+      <div className={className}>
+        { children }
+      </div>
     )
   }
 
