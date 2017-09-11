b1ec7c4a6751202cd82c6f793f2efd605e0ec8ad
Shane Morgan <shane@cd2solutions.co.uk>
2017-09-11 11:56:28 +0100

    person teaser

--- components/button.jsx
+++ components/button.jsx
@@ -10,21 +10,35 @@ import { styled, t } from 'utils/theme'
       padding: 10px 20px 12px;
       border: none;
       outline: none;
-      box-shadow: 0 2px 0 #003618;
       font-size: 1rem;
       line-height: 1.25;
       text-decoration: none;
       cursor: pointer;
       vertical-align: top;
       color: white;
+      border-radius: 5px;
       
       &.common {
         background-color: ${t('primary')};
+        &:hover {
+          color: white;
+        }
+      }
+      
+      &.small {
+        font-size: 0.9rem;
+        font-weight: 600;
+        padding: 6px 14px 7px;  
+      }
+      
+      &.secondary {
+        color: ${t('primary')};
+        border: 2px solid ${t('primary')};
+        
       }
       
       &:hover {
         filter: brightness(1.15);
-        color: white;
       }
       
     }
