70c51ff886d7bb52666f7b7f0fe4e67049f85ed8
Shane Morgan <shane@cd2solutions.co.uk>
2017-09-19 13:31:50 +0100

    loading

--- components/forms/submit.jsx
+++ components/forms/submit.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { styled, t } from 'utils/theme'
+import load from 'images/load.gif'
 
 @styled`
     position: relative;
@@ -21,18 +22,49 @@ import { styled, t } from 'utils/theme'
     &:hover {
       background-color: ${t('primaryLight')};
     }
+    ${({ submitting }) => {
+  console.log(submitting)
+  console.log(submitting)
+  console.log(submitting)
+  console.log(submitting)
+  console.log(submitting)
+  console.log(submitting)
+  console.log(submitting)
+  console.log(submitting)
+      if(submitting){
+    return(`
+        background: url(${load});
+        opacity: 0.7;
+        background-size: 100%;
+        background-repeat: no-repeat;
+        background-color: #2c2d2d;
+        background-position: 50%;
+        &:hover{
+          background-color: #2c2d2d;
+        }
+      `)}
+    }}
+
 `
+
 export default class Submit extends React.Component {
 
   static propTypes = {
     value: PropTypes.string,
     className: PropTypes.string,
+    submitting: PropTypes.bool,
+  }
+
+  static defaultProps = {
+    submitting: false
   }
 
   render() {
-    const { className='', value } = this.props
+    const { className='', value, submitting } = this.props
+    let val = value
+    if (submitting) val = ''
     return (
-      <input className={`btn ${className}`} type='submit' value={value} />
+      <input className={`btn ${className}`} type='submit' value={val} />
     )
   }
 
