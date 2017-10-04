20b8fcab2f6f860e84fc40c7db2ca86986e495d6
Alistair Richards <alistair.richards@cd2solutions.co.uk>
2017-10-04 13:12:40 +0100

    tests

--- components/tests/modal.test.js
+++ components/tests/modal.test.js
@@ -4,5 +4,4 @@ import { Modal } from '../modal'
 
 t.describe_component(Modal, { children: [] }, (c) => {
   c.has_css(`div`).has_css(`div`).with_attr(`className`, `modal`)
-  c.has_css(`Overlay`).with_attr(`visible`)
 })
