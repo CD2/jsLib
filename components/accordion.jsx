e2360f946c83e648b14382547e455e39ea31edea
Shane Morgan <shane@cd2solutions.co.uk>
2017-10-12 11:20:33 +0100

    something

--- components/accordion.jsx
+++ components/accordion.jsx
@@ -3,9 +3,10 @@ import PropTypes from 'prop-types'
 import { observer } from 'mobx-react'
 import { observable, action } from 'mobx'
 
-import { styled } from 'utils/theme'
+import { styled, t } from 'utils/theme'
 import { panel } from 'utils/common_styles'
 import decorate from 'utils/decorate'
+import { dark_panel } from "../../utils/common_styles";
 
 export class Accordion extends React.Component {
 
@@ -41,9 +42,14 @@ export class Accordion extends React.Component {
 }
 export default decorate(
   styled`
+    font-size: 0.9em;
+    p { margin: 0; }
     .accordion__header {
-      ${panel}
+      ${dark_panel}
       margin: 10px 0;
+      padding: ${t(`gutterWidth`, w=>w/2)}px;
+      font-weight: 600;
+      cursor: pointer;
     }
     .accordion__content {
       min-height: 0px;
@@ -54,6 +60,12 @@ export default decorate(
       &--open {
         min-height: 100px;
         opacity: 1;
+        position: relative;
+        top: -10px;
+        padding: 16px;
+        background: white;
+        border: 1px solid ${t('border')};
+        h3 { display: none; }
       }
     }
   `,
