bafdcd94797c1022c41993598e2c08c637f9acdf
Shane Morgan <shane@cd2solutions.co.uk>
2017-10-23 16:59:40 +0100

    article

--- components/image.jsx
+++ components/image.jsx
@@ -72,11 +72,12 @@ export default decorate(
     background-size: cover;
     background-position: 50%;
     background-repeat: no-repeat;
+    
     ${({ height }) => {
-    if(height) {
-      return `height: ${height}px;`
-    }
-  }}
+      if(height) {
+        return `height: ${height}px;`
+      }
+    }}
   `,
   Image
 )
