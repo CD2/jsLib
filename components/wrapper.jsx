a52a744e2cb64b458aed3389cc62ae7af237acc9
Matthew Pearce <matthew.pearce@cd2solutions.co.uk>
2017-11-27 17:24:38 +0000

    Fixes a bunch of stuff on tags

--- components/wrapper.jsx
+++ components/wrapper.jsx
@@ -18,6 +18,7 @@ export class Wrapper extends React.Component {
     gutter: PropTypes.number,
     innerBackground: PropTypes.string,
     noGutters: PropTypes.bool,
+    overflow: PropTypes.bool,
     overlay: PropTypes.string,
     spacing: PropTypes.oneOfType([
       PropTypes.number,
@@ -103,7 +104,7 @@ export class Wrapper extends React.Component {
           boxShadow: floating ? theme.shadow0 : `none`,
           borderRadius: floating || this.props.borderRadius ? `6px` : `0`,
           marginBottom: floating ? theme.gutterHeight.value : `0`,
-          overflow: `hidden`
+          overflow: this.props.overflow ? `initial` : `hidden`,
         }}
       >
         { this.renderOverlay() }
