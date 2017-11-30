f165d45c6f80f2dcdad88abf290b41cb43886284
Matthew Pearce <matthew.pearce@cd2solutions.co.uk>
2017-11-30 10:44:54 +0000

    Adds the route-changes store

---
+++ utils/route-changes.js
@@ -0,0 +1,28 @@
+import { observable, action, computed } from 'mobx'
+
+import { history } from 'lib/utils/router'
+
+class RouteChangesStore {
+
+  constructor() {
+    history.listen((location) => this.update(location))
+  }
+
+  @observable previousPathname = null
+  @observable routeChanges = null
+
+  @action update(location) {
+    if (location.pathname !== this.previousPathname) {
+      this.previousPathname = location.pathname
+      this.routeChanges += 1
+      window.scrollTo(0, 0)
+    }
+  }
+
+  @computed get changeCounter() {
+    return this.routeChanges
+  }
+
+}
+
+export default new RouteChangesStore()
