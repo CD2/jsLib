5bc0dd325f3036b324bbfdf85618e93f63dd24d1
Matthew Pearce <matthew.pearce@cd2solutions.co.uk>
2017-12-07 14:57:07 +0000

    Adds bulk actions to the table component

--- components/table/Table.jsx
+++ components/table/Table.jsx
@@ -2,9 +2,14 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import PaginationControls from './pagination_controls'
 
-import { observable, computed, action } from 'mobx'
+import { observable, computed, action, toJS } from 'mobx'
 import { observer, Provider } from 'mobx-react'
 import { styled, t } from 'lib/utils/theme'
+
+import Th from './Th'
+import Input from 'lib/components/forms/input'
+import Button from 'lib/components/button'
+import ModalStore from 'lib/utils/modal_store'
 
 @styled`
   font-size: 0.9em;
@@ -63,24 +68,72 @@ import { styled, t } from 'lib/utils/theme'
   .thumb-column { width: 70px; }
 `
 @observer
-export class IndexTable extends React.Component {
+export default class IndexTable extends React.Component {
 
   static propTypes = {
+    bulkActions: PropTypes.arrayOf(PropTypes.shape({
+      action: PropTypes.string.isRequired,
+      cord: PropTypes.object,
+      text: PropTypes.string,
+      formatPayload: PropTypes.func.isRequired,
+      onSuccess: PropTypes.func,
+      modalConfirmationText: PropTypes.string
+    })),
     className: PropTypes.string,
     headings: PropTypes.node,
     ids: PropTypes.array,
+    paginationPosition: PropTypes.string,
     query: PropTypes.object,
     row: PropTypes.func,
     rowProps: PropTypes.object,
   }
 
   static defaultProps = {
+    bulkActions: null,
     rowProps: {},
     ids: null,
+    paginationPosition: `bottom`,
   }
 
   @observable page=1
   @observable per_page=50
+  // bulk state
+  @observable bulkAllToggled = false
+  @observable bulkSelected = []
+
+  @action handleToggleAllBulk = () => {
+    this.bulkAllToggled = !this.bulkAllToggled
+    this.bulkSelected = this.bulkAllToggled ? this.paginated_ids : []
+  }
+
+  @action handleToggleBulkItem = id => {
+    if (this.bulkSelected.includes(id)) {
+      this.bulkSelected.replace(this.bulkSelected.filter(selectedId => selectedId !== id))
+    } else {
+      this.bulkSelected.push(id)
+    }
+  }
+
+  handleBulkActionSubmit = action => {
+    const { onSubmit, formatPayload, action: actionType, cord, onSuccess } = action
+    if (onSubmit) return onSubmit(this.bulkSelected)
+    const payload = formatPayload ? formatPayload(toJS(this.bulkSelected)) : toJS(this.bulkSelected)
+
+    cord.perform(actionType, payload).then(response => onSuccess(response))
+  }
+
+  handleBulkAction = action => {
+    if (action.modalConfirmationText) {
+      ModalStore.addItem(
+        <div>
+          <p>{action.modalConfirm}</p>
+          <Button onClick={()=>this.handleBulkActionSubmit(action)}>Proceed</Button>
+        </div>
+      )
+    } else {
+      this.handleBulkActionSubmit(action)
+    }
+  }
 
   @computed get total_items() {
     return this.props && this.props.ids ? this.props.ids.length : this.props.query.ids.length
@@ -90,12 +143,11 @@ export class IndexTable extends React.Component {
     const start = (this.page-1) * this.per_page
     const end = start + this.per_page
     const ids = this.props.ids || this.props.query.ids
+
     return ids.slice(start, end)
   }
 
-  @action handlePageChange = (page) => {
-    this.page = page
-  }
+  @action handlePageChange = (page) => this.page = page
 
   @computed get pagination_controls() {
     return (
@@ -108,28 +160,69 @@ export class IndexTable extends React.Component {
     )
   }
 
+  renderBulkAction = (action, index) => (
+    <Button key={index} onClick={() => this.handleBulkAction(action)}>
+      {action.text}
+    </Button>
+  )
+
+  renderBulkActions = () => <div>{this.props.bulkActions.map(this.renderBulkAction)}</div>
+
+  renderRow = id => {
+    let bulkColumn = null
+
+    if (this.props.bulkActions) {
+      bulkColumn = (
+        <td onClick={e => e.stopPropagation()}>
+          <Input
+            name="headerCheck"
+            type="checkbox"
+            value={this.bulkSelected.includes(id)}
+            onChange={this.handleToggleBulkItem}
+          />
+        </td>
+      )
+    }
+
+    return <this.props.row key={id} id={id} {...{ ...this.props.rowProps, bulkColumn }} />
+  }
+
+  renderBulkHeader = () => {
+    const { headings } = this.props
+    const bulkHeading = (
+      <Th>
+        <Input
+          name="headerCheck"
+          type="checkbox"
+          value={this.bulkAllToggled}
+          onChange={this.handleToggleAllBulk}
+        />
+      </Th>
+    )
+
+    return React.cloneElement(headings, headings.props, [bulkHeading, ...headings.props.children])
+  }
+
   render() {
-    const { row: Row, headings, paginationPosition } = this.props
+    const { paginationPosition, bulkActions, headings } = this.props
     return (
       <div className={this.props.className}>
-        {paginationPosition !== 'bottom' && this.pagination_controls}
+        {this.props.bulkActions ? this.renderBulkActions() : null}
+        {paginationPosition !== `bottom` && this.pagination_controls}
         <Provider query={this.props.query}>
           <div className="table__container">
             <table>
-              {headings}
+              {bulkActions ? [this.renderBulkHeader(), ...headings] : headings}
               <tbody>
-                {this.paginated_ids.map(id => {
-                  return <Row key={id} id={id} {...this.props.rowProps} />
-                })}
+                {this.paginated_ids.map(this.renderRow)}
               </tbody>
             </table>
           </div>
         </Provider>
 
-        { paginationPosition !== 'top' && this.pagination_controls}
+        { paginationPosition !== `top` && this.pagination_controls}
       </div>
     )
   }
 
 }
-export default IndexTable
