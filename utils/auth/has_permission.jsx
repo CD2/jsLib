5a6cf4c07b187174b6d54ce9de37a63f03597283
Matthew Pearce <matthew.pearce@cd2solutions.co.uk>
2017-12-08 12:40:14 +0000

    Adds a notAllowed prop to the has_permission component

--- utils/auth/has_permission.jsx
+++ utils/auth/has_permission.jsx
@@ -8,14 +8,22 @@ export class HasPermission extends React.Component {
   static propTypes = {
     auth: PropTypes.object,
     children: PropTypes.node,
+    notAllowed: PropTypes.string,
     permission: PropTypes.string,
   }
 
   render() {
-    const { auth, permission, children } = this.props
+    const { auth, permission, children, notAllowed } = this.props
     const aProps = { ...this.props }
     delete aProps.auth
     delete aProps.permission
+
+    if (notAllowed) {
+      return isAuthed(auth, notAllowed)
+        ? null
+        : React.Children.only(children)
+    }
+
     return isAuthed(auth, permission)
       ? React.Children.only(children)
       : null
