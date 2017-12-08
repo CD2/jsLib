5ab449b00e86d150dac142b77c368f21756d8da2
Alistair Richards <alistair.richards@cd2solutions.co.uk>
2017-12-08 08:42:15 +0000

    banner store

---
+++ utils/banner_store.js
@@ -0,0 +1,37 @@
+import { observable, action, computed, toJS } from 'mobx'
+
+class BannerStore {
+
+  @observable bannerContents = [];
+
+  @action set(contents) {
+    this.bannerContents = contents
+  }
+
+  @action addItem(content) {
+    this.bannerContents.push(content)
+  }
+
+  @computed get contents() {
+    return toJS(this.bannerContents)
+  }
+
+  @computed get hasContents() {
+    return this.bannerContents.length > 0
+  }
+
+  @action removeLast() {
+    this.bannerContents.pop()
+  }
+
+  getLast() {
+    return toJS(this.bannerContents.get(this.bannerContents.length-1))
+  }
+
+  @action clear() {
+    this.bannerContents = []
+  }
+
+}
+
+export default new BannerStore()
