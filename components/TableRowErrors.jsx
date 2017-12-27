f4078e63c6a70792361f0ffeeddfa81441ecde13
Alistair Richards <alistair.richards@cd2solutions.co.uk>
2017-12-27 16:05:29 +0000

    TableRowErrors

---
+++ components/TableRowErrors.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import placeholder from 'images/placeholder.png'
+import Image from 'lib/components/image'
+import FaIcon from "lib/components/fa_icon";
+import theme from 'styles/theme'
+import { observer } from 'mobx-react'
+
+@observer 
+export default class TableRowErrors extends React.Component {
+
+  static propTypes = {
+    children: PropTypes.any,
+    resource: PropTypes.object.isRequired,
+  }
+
+  renderErrorRow(){
+    console.log(`error`)
+    return(
+      <tr>
+        <td className="thumb-column"><FaIcon icon='error-outline' color={theme.error} size={1.25} /></td>
+        <td colSpan="3">
+          <span>Error loading {this.props.resource.class.name}</span>
+        </td>
+        <td />
+      </tr>
+    )
+  }
+
+  renderLoadingRow(){
+    console.log(`loading`)
+    return(
+      <tr>
+        <td className="thumb-column"><Image defaultSrc={placeholder} /></td>
+        <td><span className='placeholder large' /></td>
+        <td><span className='placeholder small' /></td>
+        <td><span className='placeholder small' /></td>
+        <td><span className='placeholder small' /></td>
+      </tr>
+    )
+  }
+
+  render(){
+    const { resource, children } = this.props
+    if (resource && resource.errored) return this.renderErrorRow()
+    if (resource && resource.loading) return this.renderLoadingRow()
+    if (resource && !resource.loaded) return this.renderLoadingRow()
+    return children
+  }
+}
