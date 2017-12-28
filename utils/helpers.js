631f8adf971a575d35127c95d5adbda0b5bb560e
Henry <hexmodr@gmail.com>
2017-12-28 17:24:41 +0000

    forms

--- utils/helpers.js
+++ utils/helpers.js
@@ -3,34 +3,6 @@ import React from 'react'
 import Constants from 'libDependencies/constants'
 
 export const title = (title) => {
-  title = Constants.SITE.NAME ? `${title} | ${Constants.SITE.NAME}` : ``
+  title += Constants.SITE.NAME ? ` | ${Constants.SITE.NAME}` : ``
   return (<title>{`${title}`}</title>)
-}
-
-export const pluralize = (count, single, plural) => {
-  return count === 1 ? single : plural || `${single}s`
-}
-
-export const simple_format = (text) => {
-  if (!text) return ``
-  return text.split(`\n`).map(p => <p key={p}>{p}</p>)
-}
-
-export const truncate = (text, { omission=`...`, length: truncate_at=20, separator }={}) => {
-  const maximum_length = truncate_at - omission.length
-  const stop = separator
-    ? text.lastIndexOf(separator, maximum_length) || maximum_length
-    : maximum_length
-  return `${text.substring(0, stop)}${omission}`
-}
-
-export const titleCase = (text) => {
-  if (!text) return ``
-  return text.
-    replace(/([^A-Z])([A-Z])/g, `$1 $2`).
-    replace(/[_-]+/g, ` `).
-    toLowerCase().
-    replace(/(^\w|\b\w)/g, (m) => { return m.toUpperCase() }).
-    replace(/\s+/g, ` `).
-    replace(/^\s+|\s+$/, ``)
 }
