d7327f03ab6178523b6cfd1462268e34d57f6f86
Alistair Richards <alistair.richards@cd2solutions.co.uk>
2018-02-07 10:27:08 +0000

    cord v3 delete button

---
+++ components/NewDeleteButton.jsx
@@ -0,0 +1,76 @@
+import React from "react"
+import PropTypes from "prop-types"
+import { observer } from "mobx-react"
+import { action } from "mobx"
+
+import { redirect } from "lib/utils/router"
+
+import Button from "lib/components/button"
+import ModalStore from "lib/utils/modal_store"
+
+@observer
+export default class DeleteButton extends React.Component {
+  static propTypes = {
+    content: PropTypes.any,
+    extraAction: PropTypes.func,
+    id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
+    message: PropTypes.string,
+    model: PropTypes.object.isRequired,
+    name: PropTypes.string,
+    redirect: PropTypes.string,
+    reloadParent: PropTypes.func,
+  }
+
+  static defaultProps = {
+    redirect: null,
+    reloadParent: null,
+  }
+
+  @action
+  handleOpenWarning = (e = null) => {
+    e && e.preventDefault()
+    e && e.stopPropagation()
+    ModalStore.addItem(this.renderModal())
+  }
+
+  handleDelete = e => {
+    e && e.preventDefault()
+    e && e.stopPropagation()
+    const { model, id, reloadParent, redirect: redirectPath } = this.props
+
+    model.perform(`destroy`, { ids: [id] }).then(() => {
+      ModalStore.removeLast()
+      reloadParent && reloadParent()
+      this.props.extraAction && this.props.extraAction()
+      if (redirectPath) redirect(redirectPath)
+    })
+  }
+
+  renderModal = () => (
+    <div style={{ textAlign: `left` }}>
+      <p>
+        {this.props.message
+          ? this.props.message
+          : `Are you sure you want to delete ${this.props.name}`}
+      </p>
+      <div>
+        <Button buttonStyle="delete" onClick={this.handleDelete}>
+          Delete
+        </Button>
+        <Button buttonStyle="cancel" onClick={() => ModalStore.removeLast()}>
+          Cancel
+        </Button>
+      </div>
+    </div>
+  )
+
+  render() {
+    return (
+      <div>
+        <Button onClick={this.handleOpenWarning}>
+          {this.props.content ? this.props.content : `Delete`}
+        </Button>
+      </div>
+    )
+  }
+}
