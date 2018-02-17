fecfeaeaa1f14d51def23910a104ad075d1bd38d
Shane Morgan <shane@cd2solutions.co.uk>
2018-02-17 15:06:25 +0000

    asd

--- components/forms/input.jsx
+++ components/forms/input.jsx
@@ -11,14 +11,12 @@ const FIELD_TYPES = {
   password_with_help: require(`./fields/password_with_help`).default,
   select: require(`./fields/select`).default,
   checkbox: require(`./fields/checkbox`).default,
-  date: require(`./fields/date`).default,
   tags: require(`./fields/tags`).default,
   radiogroup: require(`./fields/radiogroup`).default,
   image: require(`./fields/image`).default,
   hidden: require(`./fields/hidden`).default,
   country: require(`./fields/country`).default,
   range: require(`./fields/range`).default,
-  color: require(`./fields/color`).default,
   file: require(`./fields/file`).default,
 }
 
--- components/forms/submit.jsx
+++ components/forms/submit.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { styled, t } from "lib/utils/theme"
-import load from "images/load"
+import load from "images/load.gif"
 
 @styled`
     position: relative;
