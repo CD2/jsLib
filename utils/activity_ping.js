7340c37a973589adfd1fb880577d2ef31758a0b8
Alistair Richards <alistair.richards@cd2solutions.co.uk>
2018-03-05 12:42:37 +0000

    ping fix for ann

--- utils/activity_ping.js
+++ utils/activity_ping.js
@@ -1,8 +1,8 @@
 import { post } from "lib/utils/api_http"
-
+import TimeLineEntry from 'models/TimeLineEntry'
 export default function activityPing() {
   if (process.env.NODE_ENV === `development`) return null
-  post(`/api/v2/time_line_entries/perform/ping`, {
+  TimeLineEntry.perform(`ping`, {
     page: window.location.pathname,
   })
 }
