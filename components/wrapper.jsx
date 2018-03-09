b6eccd936d2f5810015c8310640e2e871b24ceaa
Shane Morgan <shane@cd2solutions.co.uk>
2018-03-09 15:50:47 +0000

    ads

--- components/wrapper.jsx
+++ components/wrapper.jsx
@@ -66,7 +66,14 @@ export class Wrapper extends React.Component {
   }
 
   render() {
-    const { backgroundImageUid, backgroundImage, background, floating, noRelative } = this.props
+    const {
+      backgroundImageUid,
+      backgroundImage,
+      background,
+      floating,
+      noRelative,
+      noMargin,
+    } = this.props
     if (backgroundImageUid || backgroundImage) {
       return (
         <Image
@@ -77,7 +84,8 @@ export class Wrapper extends React.Component {
             position: `relative`,
             boxShadow: floating ? theme.shadow0 : `none`,
             borderRadius: floating ? `6px` : `0`,
-            marginBottom: floating ? theme.gutterHeight.value : `0`,
+            marginBottom:
+              floating && !noMargin ? this.props.margin || theme.gutterHeight.value : `0`,
             overflow: this.props.overflow ? `initial` : `hidden`,
             height: this.props.fullHeight ? `100%` : `auto`,
           }}
@@ -97,7 +105,7 @@ export class Wrapper extends React.Component {
           position: noRelative ? `static` : `relative`,
           boxShadow: !this.props.noShadow && floating ? theme.shadow0 : `none`,
           borderRadius: floating || this.props.borderRadius ? `6px` : `0`,
-          marginBottom: floating ? this.props.margin || theme.gutterHeight.value : `0`,
+          marginBottom: floating && !noMargin ? this.props.margin || theme.gutterHeight.value : `0`,
           overflow: this.props.overflow ? `initial` : `hidden`,
           height: this.props.fullHeight ? `100%` : `auto`,
           border: this.props.border,
