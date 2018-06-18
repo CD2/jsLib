55aca8e96351ceafe611b274a208171875f14bbd
Alistair Richards <alistair.richards@cd2solutions.co.uk>
2018-06-18 09:11:32 +0100

    weird char

--- components/site_search/results_area.jsx
+++ components/site_search/results_area.jsx
@@ -3,7 +3,7 @@ import PropTypes from "prop-types"
 import { observer } from "mobx-react"
 import { observable, action } from "mobx"
 import { styled, t } from "lib/utils/theme"
-§
+
 import { popover } from "lib/utils/common_styles"
 import decorate from "lib/utils/decorate"
 import Result from "./Result"
